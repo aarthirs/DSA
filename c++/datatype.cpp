e6ba49a7de899081cad82c71d81d5c04bf9b8321
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-05-17 11:55:59 +0530

    Create datatype.cpp
    Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.

---
+++ c++/datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double edouble;
+    cin>>a>>b>>c>>d>>edouble;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    printf("%.3f \n %.6lf",d,edouble);
+    return 0;
+}
