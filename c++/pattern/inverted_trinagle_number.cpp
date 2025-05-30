b4a612c19c5718707abf175f714043773c695cd3
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-05-30 22:35:47 +0530

    Create inverted_trinagle_number.cpp
    Input: ‘N’ = 3
    
    Output: 
    
    1 2 3
    1 2
    1

---
+++ c++/pattern/inverted_trinagle_number.cpp
@@ -0,0 +1,10 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+       for(int j=0;j<n-i;j++){
+           cout<< j+1<<" ";
+       }
+       cout<<endl;
+
+    }
+}
