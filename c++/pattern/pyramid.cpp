844362be1e55345464ecd9f57e0ca7bfcda656b8
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-05-30 22:49:50 +0530

    Update pyramid.cpp
    Input: ‘N’ = 3
    
    Output: 
    
      *
     ***
    *****

--- c++/pattern/pyramid.cpp
+++ c++/pattern/pyramid.cpp
@@ -6,7 +6,7 @@ int main() {
     cout<<"Enter the value:";
     cin>>n;
     for (int i=0;i<n;i++){
-        for (int j=0;j<n-i;j++){
+        for (int j=1;j<n-i;j++){
             cout<<" ";
         }
         for(int j=0;j<2*i+1;j++){
