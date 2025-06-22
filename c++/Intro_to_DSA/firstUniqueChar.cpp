2432d4c6dc55d306d6d538df5b977a6cebc42dc3
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-06-22 11:22:11 +0530

    Create firstUniqueChar.cpp
    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
    
    Input 1: s = "leetcode"
    Output 1: 0
    Explanation 1: 'l' is the first non-repeating character in it which is present at index 0.

---
+++ c++/Intro_to_DSA/firstUniqueChar.cpp
@@ -0,0 +1,16 @@
+class FirstUniqueCharacter {
+public:
+	int solve(string s) {
+    		map<char,int> m; 
+            for(char ch : s){
+                  m[ch]++ ; 
+            }
+            for(int i=0;i<=s.length();i++){
+                  if(m[s[i]] == 1){
+                        return i ;
+                  }
+
+            }
+            return -1;
+      }
+};
