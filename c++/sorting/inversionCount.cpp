b1604ae914b76d861c082a7cf9b2a8a3c4dde0cc
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-09-24 08:52:03 +0530

    Update inversionCount.cpp

--- c++/sorting/inversionCount.cpp
+++ c++/sorting/inversionCount.cpp
@@ -38,3 +38,27 @@ public:
         return mergeSort(input, 0, input.size() - 1);
     }
 };
+/*
+
+mergesort:
+Each single element (base case) → returns 0.
+
+But when merging two singles, we may find inversions.
+That’s why [3,2] returns 1 in the parent call, not because the base case was non-zero, but because the merge step found 1.
+
+merge:
+
+copy  temp to arr
+arr = [2, 4, 1, 3, 5]
+Suppose we are merging [2,4] and [1,3].
+
+While merging, we build temp = [1,2,3,4].
+
+But this sorted result lives in temp, not in arr.
+
+To keep arr sorted for future recursive merges, we copy temp back into the correct slice of arr.
+
+k=1 → arr[1]=temp[0]
+k=2 → arr[2]=temp[1]
+k=3 → arr[3]=temp[2]
+*/
