7d7c113e5865262c87d7ac2098707cddca147e67
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2025-11-23 14:24:40 +0530

    Create isValidBST.cpp
    98. Given the root of a binary tree, determine if it is a valid binary search tree (BST).
    
    A valid BST is defined as follows:
    
    The left subtree of a node contains only nodes with keys strictly less than the node's key.
    The right subtree of a node contains only nodes with keys strictly greater than the node's key.
    Both the left and right subtrees must also be binary search trees.
    
    Input: root = [2,1,3]
    Output: true

---
+++ c++/Tree/isValidBST.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        // root , min ,max 
+        return valid(root,LONG_MIN, LONG_MAX);
+    }
+    bool valid(TreeNode* root , long min , long max){
+        if(!root) return true;
+
+        if(!(root->val >min && root->val <max)) return false; 
+
+        return valid(root->left,min ,root->val) && valid(root->right ,root->val , max); //left max is root and righ min is root
+    }
+};
