de7897b4b420d3d55f54da68bb3909fb1f7e9fbc
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2026-01-12 21:14:21 +0530

    Update Ksmallestpairs.cpp
    You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
    
    Define a pair (u, v) which consists of one element from the first array and one element from the second array.
    
    Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
    
     
    
    Example 1:
    
    Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
    Output: [[1,2],[1,4],[1,6]]
    Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]

--- c++/Heap/Ksmallestpairs.cpp
+++ c++/Heap/Ksmallestpairs.cpp
@@ -1,24 +1,42 @@
-class KSmallestPairs {
+class Solution {
 public:
-     vector<vector<int>> solve(vector<int> input1, vector<int> input2, int k) {
-		priority_queue<pair<int, pair<int,int>>> pq; 
-          for(int i=0;i<input1.size();i++){
-               for(int j=0;j<input2.size();j++){
-                    int sum=input1[i]+input2[j]; 
-                    if(pq.size()<k) pq.push({sum,{input1[i],input2[j]}});
-                    else if (sum < pq.top().first){
-                         pq.pop(); 
-                         pq.push({sum,{input1[i],input2[j]}});
-                    }
-               }
-          }
-          vector<vector<int>> ans;
-          while(!pq.empty()){
-               //pq(5, make_pair(10, 20))
-               ans.push_back({pq.top().second.first , pq.top().second.second});
-               pq.pop();
-          }
-           reverse(ans.begin(),ans.end());
-          return ans;
-     }
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> ans; 
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>> pq; 
+
+        //nums1 sums
+        for(int i=0; i<min(k,(int)nums1.size());i++){
+            pq.push({nums1[i]+nums2[0],i,0});
+        }
+
+        while(k-- && !pq.empty()){
+            auto [sum , i ,j]=pq.top(); 
+            pq.pop(); 
+            ans.push_back({nums1[i],nums2[j]}); 
+            //here inc j 
+            if(j+1<(int)nums2.size()){
+                 pq.push({nums1[i]+nums2[j+1],i,j+1});
+            }
+        }
+        return ans;
+    }
 };
+
+/*
+1.it is a sorted arrs :
+nums1 = [a, b, c]
+nums2 = [x, y, z]
+
+Grid of sums:
+        x       y       z
+a    a+x     a+y     a+z [--j++] 
+b    b+x     b+y     b+z
+c    c+x     c+y     c+z
+Properties:
+Each row is sorted (nums2 is sorted)
+Each column is sorted (nums1 is sorted)
+This is a sorted matrix.
+
+Because both arrays are sorted, starting with the first column (j = 0) and 
+then incrementing j step by step is sufficient to generate the k smallest pairs in correct order.
+*/
